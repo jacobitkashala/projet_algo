baf3c3f195689bcbdd39d5886a496651bb7c85e6
jacobitkashala <jacobitkashala@gmail.com>
2021-08-28 14:57:09 +0200

    add: somme de matrice

--- app.js
+++ app.js
@@ -18,21 +18,28 @@ function CamelCase(str) {
 
 function comptChar(str) {
   let comptChar = {
-   char: "",
+    char: "",
     nbr: 0
   }
   for (let i = 0; i < str.length; i++) {
     let char = str[i];
-    for (let i = count = 1; i < str.length; count += +(char === str[i++])){
-      comptChar.char=char;
-      comptChar.nbr=count;
+    for (let i = count = 1; i < str.length; count += +(char === str[i++])) {
+      comptChar.char = char;
+      comptChar.nbr = count;
     }
-    if(char !== str[i+1]){
-      console.log(comptChar.char+" "+comptChar.nbr);
+    if (char !== str[i + 1]) {
+      console.log(comptChar.char + " " + comptChar.nbr);
     }
   }
 }
 
 
+function SommeDiag(matrix) {
+  let somDiagonaleP = 0, somDiagonaleSec = 0;
 
-
+  for (let i = 0; i < matrix.length; i++) {
+    let colonne = matrix.length - i - 1;
+    somDiagonaleP += matrix[i][i];
+    somDiagonaleSec += matrix[i][colonne]
+  }
+}
